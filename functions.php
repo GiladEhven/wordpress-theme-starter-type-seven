396a05b10dd7e86ea93d521fa50e83efbed8b588
Gilad Ehven <gilad@ehven.com>
2018-10-06 23:45:38 -0400

    Added 'Theme_Core' class pattern to 'functions.php' to start simple OOP'ish theme structure.

--- functions.php
+++ functions.php
@@ -22,23 +22,48 @@
 
     if ( ! defined( 'GILAD_WEBSITE_PHASE' ) )              define( 'GILAD_WEBSITE_PHASE',              '' );
 
-	// ------------------------------------------------------------------------------- //
-	// -------------------------- ORIGINAL CHILD THEME CODE -------------------------- //
-	// ------------------------------------------------------------------------------- //
+    if ( ! class_exists( __NAMESPACE__ . 'Theme_Core' ) ) {
 
-	// Defines
-	define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
-	define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+        class Theme_Core {
 
-	// Classes
-	require_once 'classes/class-fl-child-theme.php';
+            public function __construct() {
 
-	// Actions
-	add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+				// ------------------------------------------------------------------------------- //
+				// ----------------------- OFFICIAL CHILD THEME SAMPLE CODE ---------------------- //
+				// ------------------------------------------------------------------------------- //
+
+				// Defines
+				define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
+				define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+
+				// Classes
+				require_once 'classes/class-fl-child-theme.php';
+
+				// Actions
+				add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
 
 
 
-	// ------------------------------------------------------------------------------- //
-	// ----------------------- THIS PROJECT'S CHILD THEME CODE ----------------------- //
-	// ------------------------------------------------------------------------------- //
+				// ------------------------------------------------------------------------------- //
+				// ---------------------------- GILAD'S BUILDER CHILD ---------------------------- //
+				// ------------------------------------------------------------------------------- //
 
+                if ( is_admin() ) {
+
+                    // ADMIN SIDE REQUIRES AND INSTANTIATIONS HERE
+
+                } else {
+
+                    // PUBLIC SIDE REQUIRES AND INSTANTIATIONS HERE
+
+                }
+
+                // COMMON REQUIRES AND INSTANTIATIONS HERE
+
+            }
+
+        }
+
+    }
+
+    $theme_core = new Theme_Core();
